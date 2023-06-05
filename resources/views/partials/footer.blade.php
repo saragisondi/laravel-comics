dddb67ed2c9ff178ff26ab276763ccdd40e97bb7
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-05 23:23:59 +0200

    add markup footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,6 +7,133 @@
     <title>Document</title>
 </head>
 <body>
-<h1>footer</h1>
+
+    <footer>
+        <!-- FOOTER TOP -->
+        <div class="footer-top">
+
+            <ul>
+            <li >
+                <a href="#">
+                <img src="" alt="img">
+                <span>DIGITAL COMICS</span>
+                </a>
+            </li>
+            <li >
+                <a href="#">
+                <img src="" alt="img">
+                <span>DC MERCHANDISE</span>
+                </a>
+            </li>
+            <li >
+                <a href="#">
+                <img src="" alt="img">
+                <span>SUBSCRIPTION</span>
+                </a>
+            </li>
+            <li >
+                <a href="#">
+                <img src="" alt="img">
+                <span>COMIC SHOP LOCATOR</span>
+                </a>
+            </li>
+            <li >
+                <a href="#">
+                <img src="" alt="img">
+                <span>DC POWER VISA</span>
+                </a>
+            </li>
+            </ul>
+
+        </div>
+
+        <!-- FOOTER MIDDLE -->
+        <div class="footer-middle">
+
+            <!-- left-->
+            <div class="left">
+
+            <div class="row">
+                <div class="container-list">
+                <div class="col" >
+                    <h2>DC COMICS</h2>
+                    <ul>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li> <li >
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    </ul>
+
+                    <h2 class="shop">SHOP</h2>
+                    <ul>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li >
+                        <a href="#">link</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+
+            </div>
+
+            <!-- right -->
+            <div class="right">
+
+            <div class="container-img">
+                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="logo">
+            </div>
+            </div>
+
+        </div>
+
+
+        <!-- FOOTER BOTTOM -->
+        <div class="footer-bottom">
+            <button>SIGN-UP NOW!</button>
+            <div class="social">
+            <h2>FOLLOW US</h2>
+            <div>
+                <ul>
+                <li>
+                    <img src="" alt="social">
+                </li>
+                <li>
+                    <img src="" alt="social">
+                </li>
+                <li>
+                    <img src="" alt="social">
+                </li>
+                <li>
+                    <img src="" alt="social">
+                </li>
+                <li>
+                    <img src="" alt="social">
+                </li>
+                </ul>
+            </div>
+            </div>
+        </div>
+
+        </footer>
+
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,8 +17,20 @@
         <!-- NAVBAR -->
         <nav>
             <ul>
-
+                <li>
+                    <a href="#">CHARACTERS</a>
+                    <a href="#">COMICS</a>
+                    <a href="#">MOVIES</a>
+                    <a href="#">TV</a>
+                    <a href="#">GAMES</a>
+                    <a href="#">COLLECTIBLES</a>
+                    <a href="#">VIDEOS</a>
+                    <a href="#">FANS</a>
+                    <a href="#">NEWS</a>
+                    <a href="#">SHOP</a>
+                </li>
             </ul>
+
         </nav>
 
     </header>
