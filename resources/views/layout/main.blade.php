5f2c837c996be14b1c096d068d45ea794d3a6a47
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-05 21:54:11 +0200

    add partials

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dc comics Laravel</title>
+
+    @vite('resources/js/app.js')
+
 </head>
 <body>
-<h1>{{$name}}</h1>
+
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $name = 'main';
-    return view('main', compact('name'));
-
-});
+    return view('home');
+})->name('home');
